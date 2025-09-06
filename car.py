148627acff57f51b6e9641c33b86f4c13901f592
Aizhan Dongalova <ajzandongalova@gmail.com>
2025-09-06 17:36:24 +0500

    Первый коммит: загрузка проекта

--- car.py
+++ car.py
@@ -11,13 +11,22 @@ class Car:
         self.model = model
         self.year = year
         self.odometer = 0
+        self.parking = 10
 
     # Метод экземпляра
     def drive(self, distance):
         self.odometer += distance
         return f"Driving {distance} km. Total: {self.odometer} km"
 
+    def parking (self, hour):
+        self.parking += hour
+        return f"Total parking = "
+
     #Создание объекта
-my_car = Car("Toyota", "Corolla", 2020)
-print(my_car.make) #Вывод: Toyota
-print(my_car.drive(100)) #Вывод: Driving 100 km. Total: 100 km
+my_car1 = Car("Toyota", "Corolla", 2020)
+print(my_car1.make) #Вывод: Toyota
+print(my_car1.drive(100)) #Вывод: Driving 100 km. Total: 100 km
+
+my_car2 = Car ("Nissan", "Qashqai", 2025)
+print(my_car2.drive(60))
+print(my_car2.drive(60))
--- temperature.py
+++ temperature.py
@@ -2,5 +2,18 @@ class temperature:
     # Атрибут класса
     scale_units = {"C": "Celsius", "F": "Fahrenheit"}
 
-    def __init__(selfself, degrees, scale="C"):
-        #
+    def __init__(self, degrees, scale="C"):
+        # Атрибуты экземпляра
+        self.degrees = degrees
+        self.scale = scale
+
+    # Обычный метод экземпляра
+    def convert_to(self, target_scale):
+        if self.scale == target_scale:
+            return self.degrees
+        if self.scale == "C" and target_scale == "F":
+            return (self.degrees * 9/5) +32
+        if self.scale == "F" and target_scale == "C":
+            return (self.degrees - 32) * 5/9
+my_temp = temperature(60)
+print(my_temp.convert_to("F"))
